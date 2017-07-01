875138ad010f8a3283ac37debe86e7bdafa70d61
Patrick <patrick@Patricks-Mac.local>
2017-07-01 17:01:01 -0300

    Initial

---
+++ Folha de Pagamento/Folha de Pagamento/salario.swift
@@ -0,0 +1,13 @@
+//
+//  salario.swift
+//  Folha de Pagamento
+//
+//  Created by Patrick on 01/07/17.
+//  Copyright © 2017 Patrick. All rights reserved.
+//
+
+import UIKit
+
+class salario: NSObject {
+
+}
